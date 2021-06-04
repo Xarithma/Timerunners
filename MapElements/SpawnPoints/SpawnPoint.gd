b9e2710d2d80efc667f8d4a137b3779b7134afd8
Zedespook <vasszili@gmail.com>
2021-06-04 15:29:06 +0200

    Fixed broken map generation

--- MapElements/SpawnPoints/SpawnPoint.gd
+++ MapElements/SpawnPoints/SpawnPoint.gd
@@ -5,24 +5,23 @@ export var opening: String = "Left"
 
 # When the position gets initialised.
 func _ready() -> void:
-	# Find the "Game" node
-	for game in get_tree().get_nodes_in_group("Game"):
-		# If there is already a spawned chunk, don't run further
-		if Globals.chunk_positions.has(global_position):
-			queue_free()
-
-		# Add the chunk to the array
-		Globals.chunk_positions.append(global_position)
-
-		# Spawn a room according to the point opening
-		match opening:
-			"Up":
-				Globals.spawn_chunk(Globals.up_chunks, global_position)
-			"Down":
-				Globals.spawn_chunk(Globals.down_chunks, global_position)
-			"Left":
-				Globals.spawn_chunk(Globals.left_chunks, global_position)
-			"Right":
-				Globals.spawn_chunk(Globals.right_chunks, global_position)
-
+	# If there is already a spawned chunk, don't run further
+	if Globals.chunk_positions.has(global_position):
 		queue_free()
+		return
+
+	# Add the chunk to the array
+	Globals.chunk_positions.append(global_position)
+
+	# Spawn a room according to the point opening
+	match opening:
+		"Up":
+			Globals.spawn_chunk(Globals.up_chunks, global_position)
+		"Down":
+			Globals.spawn_chunk(Globals.down_chunks, global_position)
+		"Left":
+			Globals.spawn_chunk(Globals.left_chunks, global_position)
+		"Right":
+			Globals.spawn_chunk(Globals.right_chunks, global_position)
+
+	queue_free()
